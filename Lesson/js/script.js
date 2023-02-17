dfc6d023e630484c023d7ae9866c169e08291367
Malitowska <kristina.malitowska@gmail.com>
2023-02-17 22:18:45 +0200

    add lesson

---
+++ Lesson/js/script.js
@@ -0,0 +1,94 @@
+// let elem = document.getElementById("elem");
+
+// elem.style.background = "green";
+
+// let elements = document.querySelectorAll("ul>li:last-child");
+
+// for (element of elements) {
+//   console.log(element);
+// }
+
+// let element = document.querySelector("li");
+// console.log(element);
+
+// let element = document.getElementsByTagName("div");
+// console.log(element);
+
+// let textHidden = document.getElementById("textHidden");
+// textHidden.hidden = true;
+
+// let message = document.getElementById("message");
+
+// console.log(message.value);
+
+// let text = document.querySelector(".text-message");
+// console.log(text.textContent);
+
+// let text = document.querySelector(".text-message");
+// // console.log(text.textContent);
+
+// text.textContent = "Hello, Olga!";
+
+// let btn = document.querySelector(".button");
+
+// btn.style.backgroundColor = "red";
+
+// let text = document.querySelector("#p_text");
+
+// console.log(text.classList);
+
+// console.log(text.classList.contains("red"));
+
+// text.classList.remove("red");
+
+// console.log(text.classList);
+
+// console.log(text.classList.contains("red"));
+
+// text.classList.add("new");
+
+// console.log(text.classList);
+
+// let image = document.querySelector(".image");
+
+// console.log(image.hasAttribute("src"));
+
+// console.log(image.getAttribute("src"));
+
+// const item = document.createElement("a");
+// item.href = "#";
+// item.classList.add("btn");
+// item.textContent = "3";
+
+// const nav = document.querySelector(".nav");
+// nav.appendChild(item);
+
+// const heading = document.createElement("h1");
+// heading.textContent = "Hello";
+
+// const container = document.querySelector(".conteiner");
+// container.insertBefore(heading, nav);
+
+// nav.removeChild(item);
+
+// item.remove();
+
+// const text = document.querySelector(".text");
+// const parent = document.querySelector(".parent");
+
+// const clone = text.cloneNode(true);
+
+// parent.appendChild(clone);
+
+// const container = document.querySelector(".conteiner");
+
+// const text = `<p>Sorry</p>`;
+
+// container.innerHTML += text;
+
+// const list = document.querySelector("#list");
+
+// list.insertAdjacentHTML("beforebegin", "<p>beforebegin</p>");
+// list.insertAdjacentHTML("afterbegin", "<li>afterbegin</li>");
+// list.insertAdjacentHTML("beforeend", "<li>beforeend</li>");
+// list.insertAdjacentHTML("afterend", "<p>afterend</p>");
